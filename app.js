034893cc128d791a39d1ef6e8c8de66e2d52b082
Julio Francisco Motta Claudio <jmottaclaudio@gmail.com>
2020-08-02 01:56:29 -0500

    Añadimos más data

--- app.js
+++ app.js
@@ -6,7 +6,32 @@ String: Cadenas
 "HOLA MUNDO"
 'HOLA MUNDO'
 Son la misma vaina 
+Number: Numeros 
+12
+121
+12.213
+Booleanos: Lógicos
+True 
+False 
+Array: Arreglos
+Lista de datos 
+[dato1, dato2, dato3,...,datoN]
+Objeto:
+"Juan"
+89.2
+31
+true
+Representación abstracta de un objeto en la vida real
+
+{
+    "username": "Julio",
+    "score": 89.2,
+    "hours": 31,
+    "professional": true;
+}
+
 */
+
 
 var nombre = prompt("Ingresa tu nombre");
 
