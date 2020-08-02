5c9e99b671bf012754934738690d6baf1f703219
Julio Francisco Motta Claudio <jmottaclaudio@gmail.com>
2020-08-02 01:44:37 -0500

    Añadimos más al js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 document.write("<h1>CURSO DE JAVASCRIPT</h1>")
+
+//Tipos de datos 
+/*
+String: Cadenas
+"HOLA MUNDO"
+'HOLA MUNDO'
+Son la misma vaina 
+*/
+
+var nombre = prompt("Ingresa tu nombre");
+
+if (nombre === "Julio") {
+    document.write("<h2>Bienvenido galán</h2>");
+} else {
+    document.write("<h2>You're not me</h2>");
+}
