4731419fd2338d41d110909bc4c665a76029b476
Julio Francisco Motta Claudio <jmottaclaudio@gmail.com>
2020-08-04 05:44:22 -0500

    Añadimos las iteraciones o los bucles

--- app.js
+++ app.js
@@ -32,12 +32,36 @@ Representación abstracta de un objeto en la vida real
 
 */
 
+let typeCard = "Targeta de crédito";
+
+//Condicional SWITCH
+
+switch (typeCard) {
+    case "Targeta de débito":
+        console.log("Esta es una targeta de crédito");
+        break;
+    case "Targera de crédito":
+        console.log("Esta es una targeta de crédito");
+        break;
+    default:
+        console.log("Targeta invalida");
+}
+
+//ITERADORES Y WHILES
+
+var variableContador = 0;
+
+while (variableContador < 50) { //Comparación
+    console.log(variableContador); //Acción
+    variableContador++; //Modificación de la variable de condición
+    document.write("<h3> " + variableContador + "</h3>")
+}
 
 var nombre = prompt("Ingresa tu nombre"); //El prompt es para pedir datos externos
 
 var username2 = "Julio"; //Variable
-
 let lastName = "Motta"; //Otro tipo de variable
+const pi = 3.1415; //Estos valores son constantes, no se puede cambiar 
 
 if (nombre === "Julio") {
     document.write("<h2>Bienvenido galán</h2>");
@@ -50,6 +74,8 @@ if (nombre === "Julio") {
         "time": 13.4
     });
     console.log(username2);
+    console.log(lastName);
+    lastName = "Claudio";
     console.log(lastName);
 } else {
     document.write("<h2>You're not me</h2>");
